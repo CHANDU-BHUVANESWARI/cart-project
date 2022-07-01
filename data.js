d35006a0ba48e69e1a6ef265a962d4f3c7a2ae8e
CHANDU-BHUVANESWARI <103125972+CHANDU-BHUVANESWARI@users.noreply.github.com>
2022-07-01 09:32:42 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,31 @@
+import img1 from './img1.png';
+import img2 from './img2.png';
+import img3 from './img3.png';
+
+const data = {
+    productData:[
+        {
+           id: 1,
+           img: img1,
+           title: 'Juicy Grapes',
+           desc:'',
+           price:45,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Red Watermelon',
+            desc:'',
+            price:55, 
+        },
+        {
+            id: 3,
+            img: img3,
+            title: 'Juicy Orange',
+            desc:'',
+            price:25, 
+        }
+    ],
+};
+
+export default data;
